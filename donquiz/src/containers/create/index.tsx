71a6bb397d9b88e8b1a0ca44748846d298eb139f
kmj0973 <kmj0973@naver.com>
2024-10-18 16:20:02 +0900

    Refactor: Code refactoring create quiz page, and Update firestore db save function

--- donquiz/src/containers/create/index.tsx
+++ donquiz/src/containers/create/index.tsx
@@ -6,73 +6,95 @@ import { CiImageOff } from "react-icons/ci";
 import { MdOutlineCancel } from "react-icons/md";
 import { useState } from "react";
 import Image from "next/image";
+import toast from "react-hot-toast";
+import { usePathname } from "next/navigation";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../../firebase/firebasedb";
+import { useAuthStore } from "@/hooks/useAuthStore";
+
+interface QuizList {
+  image: string;
+  answer: string;
+  source: string;
+}
 
 const Create = () => {
+  const docId = usePathname().slice(8); //documnet id
+  const uid = useAuthStore((state) => state.uid); //user id
+
   const [uploadFile, setUploadFile] = useState<File | null>(); //현재 추가한 이미지
 
   const [showImage, setShowImage] = useState<string>("");
-  const [imageList, setImageList] = useState<string[]>([]);
-  const [answerList, setAnswerList] = useState<string[]>([]);
-  const [sourceList, setSourceList] = useState<string[]>([]);
+  const [quizList, setQuizList] = useState<QuizList[]>([]);
 
   const [answer, setAnswer] = useState<string>("");
   const [source, setSource] = useState<string>("");
 
   const [showImageIndex, setShowImageIndex] = useState<number | null>();
 
+  const handleSubmitDB = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(docId);
+    if (uid && docId) {
+      const docRef = doc(db, "users", uid, "quizList", docId);
+      console.log(docRef.id);
+
+      await updateDoc(docRef, {
+        quizList: quizList,
+      });
+    }
+  };
+
   const handleAnswerAndSource = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(imageList);
-    console.log(answerList);
-    console.log(sourceList);
-    if (imageList.length == 0) {
-      console.log("사진을 등록해주세요");
+    console.log("퀴즈리스트:", quizList);
+    if (quizList.length == 0) {
+      toast.error("사진을 등록해주세요", {
+        duration: 3000,
+      });
       return;
     }
     if (answer == "" || source == "") {
-      console.log("정답과 출처를 작성해주세요");
+      toast.error("정답과 출처를 작성해주세요", {
+        duration: 3000,
+      });
       return;
     }
 
-    if (showImageIndex != null) answerList[showImageIndex] = answer;
-    setAnswerList([...answerList]);
-
-    if (showImageIndex != null) sourceList[showImageIndex] = source;
-    setSourceList([...sourceList]);
+    if (showImageIndex != null) {
+      quizList[showImageIndex].answer = answer;
+      quizList[showImageIndex].source = source;
+      setQuizList([...quizList]);
+    }
+    toast.success("저장되었습니다");
   };
 
   const handleShowImage = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     //리스트에 있는 이미지 클릭시 대표이미지 변경 및 정답, 이미지 출처 보여주기
-    const imageIndex = e.currentTarget.parentElement?.id;
-    if (imageIndex) {
-      setShowImageIndex(Number(imageIndex));
-      setShowImage(imageList[Number(imageIndex)]);
-      setAnswer(answerList[Number(imageIndex)]);
-      setSource(sourceList[Number(imageIndex)]);
+    const quizIndex = e.currentTarget.parentElement?.id;
+    if (quizIndex) {
+      setShowImageIndex(Number(quizIndex));
+      setShowImage(quizList[Number(quizIndex)].image);
+      setAnswer(quizList[Number(quizIndex)].answer);
+      setSource(quizList[Number(quizIndex)].source);
     }
   };
 
   const handleCancel = (e: React.MouseEvent<SVGAElement, MouseEvent>) => {
-    const imageIndex = e.currentTarget.parentElement?.id;
-    if (imageIndex) {
-      setImageList(imageList.filter((image, idx) => idx != Number(imageIndex)));
-      setAnswerList(
-        answerList.filter((answer, idx) => idx != Number(imageIndex))
-      );
-      setSourceList(
-        sourceList.filter((source, idx) => idx != Number(imageIndex))
-      );
+    const quizIndex = e.currentTarget.parentElement?.id;
+    if (quizIndex) {
+      setQuizList(quizList.filter((quiz, index) => index != Number(quizIndex)));
 
-      if (imageIndex !== "0" && imageList.length > 0) {
-        setShowImage(imageList[Number(imageIndex) - 1]);
-        setShowImageIndex(Number(imageIndex) - 1);
-        setAnswer(answerList[Number(imageIndex) - 1]);
-        setSource(sourceList[Number(imageIndex) - 1]);
-      } else if (imageIndex == "0" && imageList.length > 0) {
-        setShowImage(imageList[1]);
+      if (quizIndex !== "0" && quizList.length > 1) {
+        setShowImage(quizList[Number(quizIndex) - 1].image);
+        setShowImageIndex(Number(quizIndex) - 1);
+        setAnswer(quizList[Number(quizIndex) - 1].answer);
+        setSource(quizList[Number(quizIndex) - 1].source);
+      } else if (quizIndex == "0" && quizList.length > 1) {
+        setShowImage(quizList[1].image);
         setShowImageIndex(0);
-        setAnswer(answerList[1]);
-        setSource(sourceList[1]);
+        setAnswer(quizList[1].answer);
+        setSource(quizList[1].source);
       } else {
         setShowImageIndex(null);
       }
@@ -84,27 +106,31 @@ const Create = () => {
       console.log("error");
       return;
     }
-    setUploadFile(e.target.files[0]);
+    setUploadFile(e.target.files[0]); //storage에 추가하기위한 file 변수
     const reader = new FileReader();
 
     reader.readAsDataURL(e.target.files[0]);
 
     reader.onload = (e) => {
       if (reader.readyState === 2) {
+        setQuizList([
+          ...quizList,
+          { image: String(e.target!.result), answer: "", source: "" },
+        ]);
         setShowImage(String(e.target!.result));
-        setImageList([...imageList, String(e.target!.result)]);
-        setAnswerList([...answerList, ""]);
-        setSourceList([...sourceList, ""]);
+        setShowImageIndex(quizList.length);
         setAnswer("");
         setSource("");
-        setShowImageIndex(imageList.length);
       }
     };
   };
 
   return (
     <div className="w-full h-[calc(100vh-120px)] font-bold flex flex-col items-center justify-around">
-      <form className="w-[90%] flex justify-between items-center ">
+      <form
+        onSubmit={handleSubmitDB}
+        className="w-[90%] flex justify-between items-center "
+      >
         <div>
           <FaArrowLeft size="30px" />
         </div>
@@ -119,7 +145,7 @@ const Create = () => {
       <div className="w-[90%] h-[600px] flex justify-center mb-4">
         <div className="w-[100%] max-w-[600px] mr-10">
           <div className="relative h-[500px] border-4 flex justify-center items-center z-0">
-            {imageList.length != 0 ? (
+            {quizList.length != 0 ? (
               <Image src={showImage} fill alt="이미지" />
             ) : (
               <CiImageOff size="24px" />
@@ -139,27 +165,29 @@ const Create = () => {
               onChange={handleImage}
             />
             <div className="w-[70%] overflow-x-auto h-[100px] flex items-center">
-              {imageList.map((image, idx) => {
-                return (
-                  <div
-                    id={String(idx)}
-                    key={idx}
-                    className="relative w-[80px] h-[80px] flex-[0_0_auto] border-2 mx-1"
-                  >
-                    <Image
-                      key={idx}
-                      onClick={handleShowImage}
-                      src={image}
-                      fill
-                      alt="이미지"
-                    />
-                    <MdOutlineCancel
-                      onClick={handleCancel}
-                      className="absolute hover:text-red-600 right-0"
-                    />
-                  </div>
-                );
-              })}
+              {quizList.length != 0
+                ? quizList.map((quiz, idx) => {
+                    return (
+                      <div
+                        id={String(idx)}
+                        key={idx}
+                        className="relative w-[80px] h-[80px] flex-[0_0_auto] border-2 mx-1"
+                      >
+                        <Image
+                          key={idx}
+                          onClick={handleShowImage}
+                          src={quiz.image}
+                          fill
+                          alt="이미지"
+                        />
+                        <MdOutlineCancel
+                          onClick={handleCancel}
+                          className="absolute hover:text-red-600 right-0"
+                        />
+                      </div>
+                    );
+                  })
+                : null}
             </div>
           </div>
         </div>
@@ -181,7 +209,7 @@ const Create = () => {
               className="w-[250px] text-[18px] p-1 border-0 bg-[#f2f2f2] rounded-lg mb-3"
               id="answer"
               type="text"
-              value={imageList.length != 0 ? answer : ""}
+              value={quizList.length != 0 ? answer : ""}
             />
           </div>
           <div className="mb-14">
@@ -198,22 +226,9 @@ const Create = () => {
               className="w-[250px] text-[18px] p-1 border-0 bg-[#f2f2f2] rounded-lg mb-3"
               id="answer"
               type="text"
-              value={imageList.length != 0 ? source : ""}
+              value={quizList.length != 0 ? source : ""}
             />
           </div>
-          {/* <div className="mb-10">
-            <label
-              htmlFor="answer"
-              className="inline-block w-[120px] text-[20px] mr-4 text-center"
-            >
-              제한 시간(초)
-            </label>
-            <input
-              className="w-[250px] text-[18px] p-1 border-0 bg-[#f2f2f2] rounded-lg mb-3"
-              id="answer"
-              type="text"
-            />
-          </div> */}
           <button
             type="submit"
             className="w-[120px] bg-[#222222] hover:bg-black rounded-xl py-2 px-3 text-white"
