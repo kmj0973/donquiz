cf53bbdcae49eae555977a6a4512c35cfa77e8b2
kmj0973 <kmj0973@naver.com>
2024-10-26 14:56:13 +0900

    Feat: result page responsive designs

--- donquiz/src/containers/quiz/ResultDialog.tsx
+++ donquiz/src/containers/quiz/ResultDialog.tsx
@@ -38,27 +38,30 @@ const ResultDialog = ({
 
   return (
     <>
-      <div className="absolute w-screen h-screen left-0 top-0 bg-black opacity-70 flex justify-center items-center z-10"></div>
-      <div className="absolute w-screen h-screen left-0 top-0 flex justify-center items-center z-10">
+      <div className="fixed inset-0 bg-black opacity-70 flex justify-center items-center z-10"></div>
+      <div className="fixed inset-0 flex justify-center items-center z-10">
         <div
           onClick={(e) => e.stopPropagation()}
-          className="absolute w-[550px] h-[550px]
-        text-black bg-white rounded-3xl flex flex-col justify-center items-center"
+          className="relative w-[90%] max-w-[550px] h-auto sm:h-[550px] text-black bg-white rounded-3xl flex flex-col justify-center items-center p-4 sm:p-6"
         >
-          <div className="text-[50px] font-extrabold my-2 flex">
-            <Image src={CelebrateLeft} alt="결과" width="75" height="75" />
-            퀴즈 결과
-            <Image src={CelebrateRight} alt="결과" width="75" height="75" />
+          <div className="text-[32px] sm:text-[50px] font-extrabold my-4 sm:my-2 flex items-center">
+            <Image src={CelebrateLeft} alt="결과" width="65" height="65" />
+            <span className="text-[25px] sm:text-[50px] mx-2">퀴즈 결과</span>
+            <Image src={CelebrateRight} alt="결과" width="65" height="65" />
           </div>
-          <div className="flex flex-col justify-center items-center w-[450px] h-[430px] border-4 border-black mb-2 rounded-2xl">
-            <div className="text-[30px] mb-4">정답 개수 : {rightCount}개</div>
-            <div className="text-[30px] mb-4">오답 개수 : {wrongCount}개</div>
-            <div className="text-[30px] mb-14">
+          <div className="flex flex-col justify-center items-center w-full max-w-[450px] h-auto sm:h-[430px] border-4 border-black mb-2 rounded-2xl p-4 sm:p-6">
+            <div className="text-[20px] sm:text-[30px] mb-2 sm:mb-4">
+              정답 개수 : {rightCount}개
+            </div>
+            <div className="text-[20px] sm:text-[30px] mb-2 sm:mb-4">
+              오답 개수 : {wrongCount}개
+            </div>
+            <div className="text-[20px] sm:text-[30px] mb-8 sm:mb-14">
               포인트 적립 : {rightCount * 10}점
             </div>
             <Link
               onClick={handleAddPoint}
-              className="p-2 px-6 mb-2 bg-[#222222] hover:bg-black text-white text-[20px] rounded-xl"
+              className="p-2 sm:px-6 bg-[#222222] hover:bg-black text-white text-[18px] sm:text-[20px] rounded-xl"
               href="/"
               replace
             >
