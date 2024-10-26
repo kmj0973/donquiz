afbf723f2f33f79a11315d3b44b560750d1938c7
kmj0973 <kmj0973@naver.com>
2024-10-26 12:37:10 +0900

    Feat: vercel error

--- donquiz/src/containers/ranking/index.tsx
+++ donquiz/src/containers/ranking/index.tsx
@@ -59,7 +59,7 @@ const Ranking = () => {
   return (
     <div className="w-full min-h-screen overflow-auto font-bold flex flex-col items-center justify-center my-4 px-4">
       <div className="relative flex justify-around items-end gap-4 mb-8">
-        <div className="absolute top-0 right-0 text-[#F9D132] text-[24px]">
+        <div className="absolute top-0 right-0 text-[#F9D132] text-[24px] px-4 sm:px2">
           <div>1~100위</div>
         </div>
         <div className="flex flex-col justify-center items-center">
